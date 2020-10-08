7e8cf9bb8ef09bd09b093a67da47739fa4979a41
h_hoshikawa <16908935+HoshikawaHikari@users.noreply.github.com>
2020-10-09 02:36:48 +0900

    Unity用パッケージ追加

---
+++ ReversisterForUnity/Scripts/XY.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HoshihaLab.Reversister
+{
+	public class XY
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public XY(int x = 0, int y = 0)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+}
